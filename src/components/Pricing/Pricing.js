ff50049934809bfa3863eb28174e9de88b47b61f
Albert Flynn <110384661+albertflynn2@users.noreply.github.com>
2023-02-15 23:25:10 +0000

    Add files via upload

---
+++ src/components/Pricing/Pricing.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Button, Heading, TextWrapper } from '../../globalStyles';
+import { useNavigate } from "react-router-dom";
+import { IconContext } from 'react-icons/lib';
+import { Link } from 'react-router-dom';
+import {
+	PricingSection,
+	PricingWrapper,
+	PricingContainer,
+	PricingCardInfo,
+	PricingCardPlan,
+	PricingCardCost,
+	PricingCardFeatures,
+	PricingCardText,
+	PricingCardFeature,
+	PricingCard,
+} from './PricingStyles';
+import { pricingData } from '../../data/PricingData';
+
+function Pricing() {
+
+	let navigate = useNavigate(); 
+	const routeChange = () =>{ 
+	  let path = `/contact`; 
+	  navigate(path);
+	}
+	
+	return (
+		<IconContext.Provider value={{ color: '#95CE59', size: '1rem' }}>
+			<PricingSection id="pricing">
+				<PricingWrapper>
+					<Heading>Treatment</Heading>
+
+					<TextWrapper
+						mb="1.4rem"
+						weight="600"
+						size="1.1rem"
+						color="white"
+						align="center"
+					>
+						Costs
+					</TextWrapper>
+					<PricingContainer>
+						{pricingData.map((card, index) => (
+							<PricingCard key={index}>
+								<PricingCardInfo>
+									<PricingCardPlan>{card.title}</PricingCardPlan>
+									<PricingCardCost>{card.price}</PricingCardCost>
+									<PricingCardText>{card.description}</PricingCardText>
+									<PricingCardFeatures>
+										{card.features.map((feature, index) => (
+											<PricingCardFeature key={index}>
+												{feature}
+											</PricingCardFeature>
+										))}
+									</PricingCardFeatures>
+									<Button onClick={routeChange}>Get Started</Button>
+								</PricingCardInfo>
+							</PricingCard>
+						))}
+					</PricingContainer>
+				</PricingWrapper>
+			</PricingSection>
+		</IconContext.Provider>
+	);
+}
+export default Pricing;
